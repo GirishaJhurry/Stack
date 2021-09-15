39865740d54ab8b6fc33e42c6c6b6367a5fa7af7
Girisha Jhurry <Girisha.Jhurry@ceridian.com>
2021-09-15 09:50:06 +0400

    Add project files.

---
+++ Stack/CustomStack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    class CustomStack
+    {
+        public int top;
+        public string[] items = new string[10];
+        public int internalIndex = -1;
+
+        public void Push(string word)
+        {
+            items[++internalIndex] = word;
+        }
+
+        public string Pop()
+        {
+            return items[internalIndex--];
+        }
+
+        public int Count()
+        {
+            return internalIndex + 1;
+        }
+    }
+}
---
+++ Stack/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    class Program 
+    { 
+        static void Main(string[] args)
+        {
+            CustomStack stack = new CustomStack();
+
+            stack.Push("Jedi");
+            stack.Push("Skillslab");
+
+            while (stack.Count()>0)
+            {
+                var item = stack.Pop();
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
